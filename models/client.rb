6297fd065383f070cca71a35ddd9f926f0d3cf21
chloe alcorn <ca.alcorn@outlook.com>
2018-08-24 17:58:53 +0100

    delete all method added for clients and works

--- models/client.rb
+++ models/client.rb
@@ -16,34 +16,38 @@ class Client
     return "#{@first_name} #{@last_name}"
   end
 
-def save()
-sql = "INSERT INTO clients
-      (first_name,last_name,age)
-      VALUES($1,$2,$3) RETURNING id"
-values = [@first_name,@last_name,@age]
-result = SqlRunner.run(sql,values)
-@id = result.first()['id'].to_i
-end
+  def save()
+    sql = "INSERT INTO clients
+    (first_name,last_name,age)
+    VALUES($1,$2,$3) RETURNING id"
+    values = [@first_name,@last_name,@age]
+    result = SqlRunner.run(sql,values)
+    @id = result.first()['id'].to_i
+  end
 
-def update()
-sql = "UPDATE clients
-      SET(first_name,last_name,age) =
-          ($1,$2,$3) WHERE id = $4"
-values =[@first_name,@last_name,@age,@id]
-SqlRunner.run(sql,values)
-end
+  def update()
+    sql = "UPDATE clients
+    SET(first_name,last_name,age) =
+    ($1,$2,$3) WHERE id = $4"
+    values =[@first_name,@last_name,@age,@id]
+    SqlRunner.run(sql,values)
+  end
 
-def delete()
-  sql = "DELETE FROM clients WHERE id = $1"
-  values =[@id]
-  SqlRunner.run(sql,values)
-end
+  def delete()
+    sql = "DELETE FROM clients WHERE id = $1"
+    values =[@id]
+    SqlRunner.run(sql,values)
+  end
 
-def self.all()
-  sql = "SELECT * FROM clients"
-  clients = SqlRunner.run(sql)
-  return clients.map{ |client| Client.new(client)}
-end
+  def self.all()
+    sql = "SELECT * FROM clients"
+    clients = SqlRunner.run(sql)
+    return clients.map{ |client| Client.new(client)}
+  end
 
+  def self.delete_all()
+    sql = "DELETE * FROM clients"
+    SqlRunner.run(sql)
+  end
 
 end
